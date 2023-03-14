296ee8f19d9a83de5b76161c434e4827c1a35073
Michael Woodard <michael.woodard1@bp.com>
2023-03-15 09:02:59 +1300

    Initial Commit

---
+++ littlelemon/littlelemonApp.swift
@@ -0,0 +1,20 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Michael Woodard on 15/03/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
